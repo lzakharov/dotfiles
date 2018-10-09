8f157b0261967104ed89cb1ddc81b0af97fdc4c3
Lev Zakharov <l.j.zakharov@gmail.com>
2018-10-09 14:56:37 +0300

    Add installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Zsh
+ln -sf ~/.config/.zshrc ~/.zshrc
+ln -sf ~/.config/.zshenv ~/.zshenv 
+
+# Git
+ln -sf ~/.config/.gitconfig ~/.gitconfig 
+ln -sf ~/.config/.gitignore_global ~/.gitignore_global 
+
+# Emacs
+ln -sf ~/.config/.emacs/init.el ~/.emacs.d/init.el
